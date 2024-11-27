a0eed5142effa99fdbbcd3353ba027928f328a54
Kalo Jo <kalojo@Kalos-MBP.localdomain>
2024-11-27 16:16:05 -0700

    Okay now that I can actually see my local host I've edited a couple of things for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,63 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import spring from "./images/Spring.jpg";
+import summer from "./images/Summer.jpg";
+import fall from "./images/Fall.jpg";
+import winter from "./images/Winter.jpg";
 import "./App.css";
-import { Navigate } from "react-router-dom";
 
 function App() {
-  if (GoToFall) {
-    return <Navigate to="/fall" />;
-  }
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <h1>Seventy Shades</h1>
-      <main>
-        <body>
-          <div>Select Your Personal Season</div>
-          <nav>
-            <a href="./Pages/spring.html">
-              {" "}
-              <button>Spring</button>
-            </a>
-            <a href="./Pages/summer.html">
-              {" "}
-              <button>Summer</button>
-            </a>
-            <a href="./Pagesfall.html">
-              {" "}
-              <button
-                onClick={() => {
-                  setGoToFall(true);
-                }}
-              >
-                Fall
-              </button>
-            </a>
-            <a href="./Pages/winter.html">
-              {" "}
-              <button>Winter</button>
-            </a>
-          </nav>
-          <img
-            class="Spring"
-            src="./images/Spring.jpg"
-            placeholder="Spring"
-            alt="./Spring_Img"
-            width="90"
-            height="120"
-          ></img>
-          <img
-            class="Summer"
-            src="./images/Summer.jpg"
-            placeholder="Summer"
-            alt="Spring_Img"
-            width="90"
-            height="120"
-          ></img>
-          <img
-            class="Fall"
-            src="./images/Fall.jpg"
-            placeholder="Fall"
-            alt="Spring_Img"
-            width="90"
-            height="120"
-          ></img>
-          <img
-            class="Winter"
-            src="./images/Winter.jpg"
-            placeholder="Winter"
-            alt="Spring_Img"
-            width="90"
-            height="120"
-          ></img>
-        </body>
-      </main>
       <div>
-        <a href="./Pages/login.html">
-          {" "}
-          <button>LOGIN</button>
-        </a>
+        <img
+          className="Spring"
+          placeholder="Spring"
+          src={spring}
+          alt="Spring_Img"
+          width="90"
+          height="120"
+        />
+
+        <img
+          className="Summer"
+          src={summer}
+          placeholder="Summer"
+          alt="Summer_Img"
+          width="90"
+          height="120"
+        />
+        <img
+          className="Fall"
+          src={fall}
+          placeholder="Fall"
+          alt="Fall_Img"
+          width="90"
+          height="120"
+        />
+        <img
+          className="Winter"
+          src={winter}
+          placeholder="Winter"
+          alt="Winter_Img"
+          width="90"
+          height="120"
+        />
       </div>
-      <footer>All of Your Seasonal Colors in One Place</footer>
-      <div>
-        <a
-          style="width: 50%; float:center; margin: .5pc;"
-          class="LINKS"
-          href="https://github.com/KdotJo/SeventyShades"
-        >
-          Github
-        </a>
-        <a
-          style="width: 50%; float:center; margin: .5pc;"
-          class="WEBSOCKET"
-          href="./Pages/WebSocket.html"
-        >
-          {" "}
-          Websocket
-        </a>
-        <a
-          style="width: 50%; float:center; margin: .5pc;"
-          class="DB"
-          href="./Pages/DB.html"
-        >
-          {" "}
-          Database
-        </a>
-        <a
-          style="width: 50%; float:center; margin: .5pc;"
-          class="ABTUS"
-          href="./Pages/About_us.html"
-        >
-          {" "}
-          About Us
-        </a>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
